a69b977d7b4f3b76e7cbdcf4d7f6f8f9dcc32de5
citeFred <citefred@gmail.com>
2025-11-17 16:48:00 +0900

    feat: Ready to use ORM in the detection resources

--- src/detection/detection.module.ts
+++ src/detection/detection.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
 import { DetectionService } from './detection.service';
 import { DetectionController } from './detection.controller';
+// 1. TypeOrmModule, Detection 임포트
+import { Detection } from './entities/detection.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  // 2. forFeature()로 이 모듈에서 사용할 Entity를 등록
+  imports: [TypeOrmModule.forFeature([Detection])],
   controllers: [DetectionController],
   providers: [DetectionService],
 })
