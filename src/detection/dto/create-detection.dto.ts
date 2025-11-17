cb7e039e5ce4641d007669a07488652995cac7d4
citeFred <citefred@gmail.com>
2025-11-17 17:34:13 +0900

    refactor: Add Validation Pipe Globally

--- src/detection/dto/create-detection.dto.ts
+++ src/detection/dto/create-detection.dto.ts
@@ -1,1 +1,13 @@
-export class CreateDetectionDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class CreateDetectionDto {
+  // 2. 'filename' 속성 정의
+  @IsString() // 이 값은 '문자열'이어야 한다
+  @IsNotEmpty() // 이 값은 '빈 값'일 수 없다
+  filename: string;
+
+  // 3. 'filetype' 속성 정의
+  @IsString()
+  @IsNotEmpty()
+  filetype: string;
+}
--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+// 1. ValidationPipe 임포트
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // 2. Global Pipe로 ValidationPipe 추가
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // DTO에 정의되지 않은 속성은 자동으로 제거
+    transform: true, // 요청 데이터를 DTO 클래스 인스턴스로 변환
+  }));
+
+  await app.listen(3000);
 }
 bootstrap();
